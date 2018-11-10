964e74ba1bfd32c2f3c33a206c5a427d02e3fbf4
James Ives <iam@jamesiv.es>
2018-11-10 18:46:06 -0500

    Blizzard API Migrate and WoW Tokens (#44)
    * Migrates API Version
    
    * Error Handling v1
    
    * Migrating to aiohttp from requests
    
    Moving to aiohttp from requests
    
    * Consistent Quotes
    
    * Additional Changes
    
    * WoW token
    
    * app.json Adjustments

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ With this bot you're able to lookup a [World of Warcraft](http://worldofwarcraft
 
 ## Installation Steps :minidisc: 
 
-1. Visit the [Blizzard API website](https://dev.battle.net/) and sign up for an API key.
+1. Visit the [Blizzard API website](https://dev.battle.net/) and sign up for an API client id/secret.
 2. Visit the [Discord API website and create an application](https://discordapp.com/developers/applications/). You'll need to create a bot user and retrieve the [bot token](https://discordapp.com/developers/docs/intro#bots-and-apps) it provides.
 3. Invite the bot user to your server using the following URL by replacing the `YOUR_CLIENT_SECRET` portion with the one found within your [Discord API settings](https://discordapp.com/developers/applications/): `https://discordapp.com/oauth2/authorize?&client_id=YOUR_CLIENT_ID_HERE&scope=bot&permissions=0`
 4. Click the button below and enter the required fields to deploy the bot to [Heroku](http://heroku.com). If the required tokens were provided correctly the bot should appear online within Discord and begin responding to commands. 
@@ -32,7 +32,8 @@ The installation will require you to enter a number of API keys and settings as 
 
 | Key  | Value Information | Required |
 | ------------- | ------------- | ------------- |
-| `WOW_API_KEY`  | Required to make calls to the Blizzard API, you can sign up for a key [here](https://dev.battle.net/).  | **Yes** |
+| `WOW_CLIENT_ID`  | Required to make calls to the Blizzard API, you can sign up for a client id [here](https://dev.battle.net/).  | **Yes** |
+| `WOW_CLIENT_SECRET`  | Required to make calls to the Blizzard API, you can sign up for a client secret [here](https://dev.battle.net/).  | **Yes** |
 | `WOW_REGION`  | The server region you'd like to query, for example `us`, or `eu`.  | **Yes** |
 | `LOCALE`  | The language for your selected WoW region, for example `en_US`, or `en_GB`. Locale depends on region. Please refer to the [Blizzard API documents](https://dev.battle.net/) for more information. At this time this bot will only return data in English.   | **Yes** |
 | `DISCORD_BOT_TOKEN`  | The token for your Discord bot user, you can sign up for one [here](https://discordapp.com/developers/docs/intro). | **Yes** |
@@ -55,6 +56,10 @@ The following commands are accepted by the bot.
 # You can also provide an optional region to each query to display players from other WoW regions outside of the bot default, for example EU, US, etc.
 !armory pve <name> <realm> <region>
 !armory pvp <armory-link> <region>
+
+# Displays the currnet wow token price.
+!armory token
+!armory token <region>
 
 # Command list/help
 !armory help
--- app.py
+++ app.py
@@ -13,33 +13,63 @@ client = discord.Client()
 @client.event
 async def on_message(message):
     """Listens for specific user messages."""
-    bot_error = 'Could not find a player with that name, realm or region combination. Type `!armory help` for more a list of valid commands.'
+    # Current time (Used for cache busting character thumbnails).
+    epoch_time = int(time.time())
 
     # If the author is the bot do nothing.
     if message.author == client.user:
         return
 
-    # If the author is not the bot, and the message starts with '!armory pve', display the characters PVE data sheet.
+    if message.content.startswith('!armory token'):
+        split = split_query(message.content, 'wow_token')
+        region = split[0]
+        info = await wow_token_price(region)
+
+        # Returns a message to the channel if there's an error fetching.
+        if info == 'not_found':
+            msg = GOLD_ERROR.format(message)
+            await client.send_message(message.channel, msg)
+
+        elif info == 'connection_error':
+            msg = CONNECTION_ERROR.format(message)
+            await client.send_message(message.channel, msg)
+
+        elif info == 'credential_error':
+            msg = CREDENTIAL_ERROR.format(message)
+            await client.send_message(message.channel, msg)
+
+        else:
+            msg = '`The current price of a WoW Token on %s realms is %s gold.` :moneybag:' % (region, info)
+            await client.send_message(message.channel, msg)
+
+
     if message.content.startswith('!armory pve'):
-        # Sends the query to be split via a utility function. Accepts either a character/realm, or an armory link.
         split = split_query(message.content, 'pve')
 
         # Assigns the 3rd index in the split to the region
         region = split[3]
 
         # Sends the returned data to the character_info function to build a character sheet.
-        info = character_info(split[0], split[1], split[2], region)
+        info = await character_info(split[0], split[1], split[2], region)
 
-        # If the returned data is an empty string send a message saying the player/realm/region couldn't be found.
-        if info == '':
-            msg = bot_error.format(message)
+        # Returns a message to the channel if there's an error fetching.
+        if info == 'not_found':
+            msg = NOT_FOUND_ERROR.format(message)
             await client.send_message(message.channel, msg)
 
-        # Otherwise respond with an incredibly long string of data holding all of the info.
-        else:
-            # Current time (Used for cache busting character thumbnails).
-            epoch_time = int(time.time())
+        elif info == 'connection_error':
+            msg = CONNECTION_ERROR.format(message)
+            await client.send_message(message.channel, msg)
 
+        elif info == 'credential_error':
+            msg = CREDENTIAL_ERROR.format(message)
+            await client.send_message(message.channel, msg)
+
+        elif info == 'unknown_error':
+            msg = UNKNOWN_ERROR.format(message)
+            await client.send_message(message.channel, msg)
+
+        else:
             # Format the AOTC/CE strings if they exist.
             ud_feat = ''
 
@@ -47,31 +77,31 @@ async def on_message(message):
                 ud_feat = '**`%s`**' % (info['ud_feat'])
 
             msg = discord.Embed(
-                title="%s" % (info['name']),
+                title='%s' % (info['name']),
                 colour=discord.Colour(info['class_colour']),
-                url="%s" % (info['armory']),
-                description="%s %s %s %s" % (
+                url='%s' % (info['armory']),
+                description='%s %s %s %s' % (
                     info['level'], info['faction'], info['spec'], info['class_type']))
             msg.set_thumbnail(
-                url="https://render-%s.worldofwarcraft.com/character/%s?_%s" % (
+                url='https://render-%s.worldofwarcraft.com/character/%s?_%s' % (
                     region, info['thumb'], epoch_time))
             msg.set_footer(
-                text="!armory help | Feedback: https://github.com/JamesIves/discord-wow-armory-bot/issues",
-                icon_url="https://raw.githubusercontent.com/JamesIves/discord-wow-armory-bot/master/assets/icon.png")
+                text='!armory help | Feedback: https://github.com/JamesIves/discord-wow-armory-bot/issues',
+                icon_url='https://raw.githubusercontent.com/JamesIves/discord-wow-armory-bot/master/assets/icon.png')
             msg.add_field(
-                name="Character",
-                value="**`Name`:** `%s`\n**`Realm`:** `%s (%s)`\n**`Item Level`:** `%s`" % (
+                name='Character',
+                value='**`Name`:** `%s`\n**`Realm`:** `%s (%s)`\n**`Item Level`:** `%s`' % (
                     info['name'], info['realm'], region.upper(), info['ilvl']),
                 inline=True)
             msg.add_field(
-                name="Keystone Achievements",
-                value="**`Master(+15)`:** `%s`\n**`Conqueror(+10)`:** `%s` \n**`Challenger(+5)`:** `%s`" % (
+                name='Keystone Achievements',
+                value='**`Master(+15)`:** `%s`\n**`Conqueror(+10)`:** `%s` \n**`Challenger(+5)`:** `%s`' % (
                     info['keystone_master'], info['keystone_conqueror'],
                     info['keystone_challenger']),
                 inline=True)
             msg.add_field(
-                name="Uldir",
-                value="**`Normal`:** `%s/%s`\n**`Heroic`:** `%s/%s`\n**`Mythic`:** `%s/%s`\n%s" % (
+                name='Uldir',
+                value='**`Normal`:** `%s/%s`\n**`Heroic`:** `%s/%s`\n**`Mythic`:** `%s/%s`\n%s' % (
                     info['uldir']['normal'], info['uldir']['bosses'],
                     info['uldir']['heroic'], info['uldir']['bosses'],
                     info['uldir']['mythic'], info['uldir']['bosses'],
@@ -85,67 +115,78 @@ async def on_message(message):
     if message.content.startswith('!armory pvp'):
         split = split_query(message.content, 'pvp')
         region = split[3]
-        info = character_info(split[0], split[1], split[2], split[3])
+        info = await character_info(split[0], split[1], split[2], split[3])
 
-        if info == '':
-            msg = bot_error.format(message)
+        if info == 'not_found':
+            msg = NOT_FOUND_ERROR.format(message)
+            await client.send_message(message.channel, msg)
+
+        elif info == 'connection_error':
+            msg = CONNECTION_ERROR.format(message)
+            await client.send_message(message.channel, msg)
+
+        elif info == 'credential_error':
+            msg = CREDENTIAL_ERROR.format(message)
+            await client.send_message(message.channel, msg)
+
+        elif info == 'unknown_error':
+            msg = UNKNOWN_ERROR.format(message)
             await client.send_message(message.channel, msg)
 
         else:
-
             msg = discord.Embed(
-                title="%s" % (info['name']),
+                title='%s' % (info['name']),
                 colour=discord.Colour(info['class_colour']),
-                url="%s" % (info['armory']),
-                description="%s %s %s %s" % (
+                url='%s' % (info['armory']),
+                description='%s %s %s %s' % (
                     info['level'], info['faction'], info['spec'], info['class_type']))
             msg.set_thumbnail(
-                url="https://render-%s.worldofwarcraft.com/character/%s" % (
-                    region, info['thumb']))
+                url='https://render-%s.worldofwarcraft.com/character/%s?_%s' % (
+                    region, info['thumb'], epoch_time))
             msg.set_footer(
-                text="!armory help | Feedback: https://github.com/JamesIves/discord-wow-armory-bot/issues",
-                icon_url="https://github.com/JamesIves/discord-wow-armory-bot/blob/master/assets/icon.png?raw=true")
+                text='!armory help | Feedback: https://github.com/JamesIves/discord-wow-armory-bot/issues',
+                icon_url='https://github.com/JamesIves/discord-wow-armory-bot/blob/master/assets/icon.png?raw=true')
             msg.add_field(
-                name="Character",
-                value="**`Name`:** `%s`\n**`Realm`:** `%s (%s)`\n**`Battlegroup`:** `%s`\n**`Item Level`:** `%s`" % (
+                name='Character',
+                value='**`Name`:** `%s`\n**`Realm`:** `%s (%s)`\n**`Battlegroup`:** `%s`\n**`Item Level`:** `%s`' % (
                     info['name'], info['realm'], region.upper(), info['battlegroup'], info['ilvl']),
                 inline=True)
             msg.add_field(
-                name="Arena Achievements",
-                value="**`Challenger`:** `%s`\n**`Rival`:** `%s`\n**`Duelist`:** `%s`\n**`Gladiator`:** `%s`" % (
+                name='Arena Achievements',
+                value='**`Challenger`:** `%s`\n**`Rival`:** `%s`\n**`Duelist`:** `%s`\n**`Gladiator`:** `%s`' % (
                     info['arena_challenger'], info['arena_rival'],
                     info['arena_duelist'], info['arena_gladiator']),
                 inline=True)
             msg.add_field(
-                name="RBG Achievements",
-                value="**`%s`:** `%s`\n**`%s`:** `%s`\n**`%s`:** `%s`" % (
+                name='RBG Achievements',
+                value='**`%s`:** `%s`\n**`%s`:** `%s`\n**`%s`:** `%s`' % (
                     info['rbg_2400_name'], info['rbg_2400'],
                     info['rbg_2000_name'], info['rbg_2000'],
                     info['rbg_1500_name'], info['rbg_1500']),
                 inline=True)
             msg.add_field(
-                name="Rated 2v2",
-                value="**`Rating`:** `%s`" % (
+                name='Rated 2v2',
+                value='**`Rating`:** `%s`' % (
                     info['2v2']),
                 inline=True)
             msg.add_field(
-                name="Rated 3v3",
-                value="**`Rating`:** `%s`" % (
+                name='Rated 3v3',
+                value='**`Rating`:** `%s`' % (
                     info['3v3']),
                 inline=True)
             msg.add_field(
-                name="Rated Battlegrounds",
-                value="**`Rating`:** `%s`" % (
+                name='Rated Battlegrounds',
+                value='**`Rating`:** `%s`' % (
                     info['rbg']),
                 inline=True)
             msg.add_field(
-                name="2v2 Skirmish",
-                value="**`Rating`:** `%s`" % (
+                name='Skirmish 2v2',
+                value='**`Rating`:** `%s`' % (
                     info['2v2s']),
                 inline=True)
             msg.add_field(
-                name="Lifetime Honorable Kills",
-                value="`%s`" % (
+                name='Lifetime Honorable Kills',
+                value='`%s`' % (
                     info['kills']),
                 inline=True)
 
@@ -164,12 +205,16 @@ async def on_message(message):
             !armory pvp <name> <realm>
             !armory pvp <armory-link>
 
+            # Displays the WoW token price
+            !armory token
+
             # You can also provide an optional region to each query to display players from other WoW regions outside of the bot default, for example EU, US, etc.
             !armory pve <name> <realm> <region>
             !armory pvp <armory-link> <region>
+            !armory token <region>
 
             ```
-            • Bot created by James Ives (jamesiv.es)
+            • Bot created by James Ives (https://jamesiv.es)
             • Feedback, Issues and Source: https://github.com/JamesIves/discord-wow-armory-bot/issues
             """
 
@@ -179,8 +224,8 @@ async def on_message(message):
 
 @client.event
 async def on_ready():
-    if WOW_API_KEY == '':
-        print('Missing World of Warcraft API key. Please refer to https://github.com/JamesIves/discord-wow-armory-bot#configuration for more details')
+    if WOW_CLIENT_ID == '' or WOW_CLIENT_SECRET == '':
+        print('Missing World of Warcraft Client ID/Secret. Please refer to https://github.com/JamesIves/discord-wow-armory-bot#configuration for more details')
 
     if WOW_REGION == '':
         print('Missing World of Warcraft player region. Please refer to https://github.com/JamesIves/discord-wow-armory-bot#configuration for more details')
